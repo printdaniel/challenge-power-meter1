6c4c32055d610bd10f984a53ae2eed05a2348e99
Daniel Suárez <dwsuarez98@gmail.com>
2023-01-12 17:49:40 -0300

    endopoit medicion máxima arreglado

--- projects/urls.py
+++ projects/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import MedidorViewSet, MedicionViewSet
+from .max_med import MedicionMaximaView
 
 router = routers.DefaultRouter()
 router.register('medidores', MedidorViewSet)
@@ -8,4 +9,5 @@ router.register('mediciones', MedicionViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('mediciones-maximas/<str:llave>/', MedicionMaximaView.as_view()),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets, serializers
+from rest_framework.views import APIView
 from .models import Medidor, Medicion
 
 class MedidorSerializer(serializers.ModelSerializer):
@@ -18,3 +19,10 @@ class MedicionSerializer(serializers.ModelSerializer):
 class MedicionViewSet(viewsets.ModelViewSet):
     queryset = Medicion.objects.all()
     serializer_class = MedicionSerializer
+
+
+class MedicionMaximaView(APIView):
+    def get(self, request, medidor_id):
+        medicion_maxima = Medicion.objects.filter(medidor=medidor_id).order_by('-consumo').first()
+        serializer = MedicionSerializer(medicion_maxima)
+        return Response(serializer.data)
