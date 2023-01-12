d494a0788fe1d3dc1bb1550b741f70c45d0b2053
Daniel Suárez <dwsuarez98@gmail.com>
2023-01-12 18:40:02 -0300

    Consumo total con metodo Sum

--- projects/mediciones_consumo.py
+++ projects/mediciones_consumo.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Medicion, Medidor
 from .views import MedicionSerializer
+from django.db.models import Sum
 
 class MedicionMaximaView(APIView):
     def get(self, request, llave):
@@ -18,5 +19,15 @@ class MedicionMinimaView(APIView):
             medicion_minima = Medicion.objects.filter(medidor=medidor).order_by('consumo').first()
             serializer = MedicionSerializer(medicion_minima)
             return Response(serializer.data)
+        except Medidor.DoesNotExist:
+            return Response({"error":"Medidor con llave {} no existe".format(llave)}, status=status.HTTP_404_NOT_FOUND)
+
+
+class ConsumoTotalView(APIView):
+    def get(self, request, llave):
+        try:
+            medidor = Medidor.objects.get(llave=llave)
+            consumo_total = Medicion.objects.filter(medidor=medidor).aggregate(Sum('consumo'))
+            return Response(consumo_total)
         except Medidor.DoesNotExist:
             return Response({"error":"Medidor con llave {} no existe".format(llave)}, status=status.HTTP_404_NOT_FOUND)
--- projects/urls.py
+++ projects/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import MedidorViewSet, MedicionViewSet
-from .mediciones_consumo import MedicionMaximaView, MedicionMinimaView
+from .mediciones_consumo import MedicionMaximaView, MedicionMinimaView, ConsumoTotalView
 
 router = routers.DefaultRouter()
 router.register('medidores', MedidorViewSet)
@@ -11,4 +11,5 @@ urlpatterns = [
     path('', include(router.urls)),
     path('mediciones-maximas/<str:llave>/', MedicionMaximaView.as_view()),
     path('mediciones-minimas/<str:llave>/', MedicionMinimaView.as_view()),
+    path('consumo-total/<str:llave>/', ConsumoTotalView.as_view()),
 ]
