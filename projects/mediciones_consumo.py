93291e526fee78f7e0aa38c59b5a50abfb1a736d
Daniel Suárez <dwsuarez98@gmail.com>
2023-01-12 21:31:10 -0300

    except agregados

--- projects/mediciones_consumo.py
+++ projects/mediciones_consumo.py
@@ -1,15 +1,19 @@
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework import status
 from .models import Medicion, Medidor
 from .views import MedicionSerializer
 from django.db.models import Sum, Avg
 
 class MedicionMaximaView(APIView):
     def get(self, request, llave):
-        medidor = Medidor.objects.get(llave=llave)
-        medicion_maxima = Medicion.objects.filter(medidor=medidor).order_by('-consumo').first()
-        serializer = MedicionSerializer(medicion_maxima)
-        return Response(serializer.data)
+        try:
+            medidor = Medidor.objects.get(llave=llave)
+            medicion_maxima = Medicion.objects.filter(medidor=medidor).order_by('-consumo').first()
+            serializer = MedicionSerializer(medicion_maxima)
+            return Response(serializer.data)
+        except Medidor.DoesNotExist:
+            return Response({"error":"Medidor con llave {} no existe".format(llave)}, status=status.HTTP_404_NOT_FOUND)
 
 
 class MedicionMinimaView(APIView):
--- projects/views.py
+++ projects/views.py
@@ -17,12 +17,7 @@ class MedicionSerializer(serializers.ModelSerializer):
         model = Medicion
         fields = ('medidor', 'fecha_hora', 'consumo')
 
-    def __str__(self):
-        return self.nombre
 
 class MedicionViewSet(viewsets.ModelViewSet):
     queryset = Medicion.objects.all()
     serializer_class = MedicionSerializer
-
-
-
